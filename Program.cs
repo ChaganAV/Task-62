4c55e35f272cef36fe74f5e15fe839d2a9b51d3a
Alexandr <jwchaganav@gmail.com>
2022-11-23 18:54:45 +0400

    проверил на N=5

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
 ﻿int N = 4;
-string[,] matrix = new string[N,N]; // массив
+//string[,] matrix = new string[N,N]; // массив
 int[,] mas = new int[N,N];
-int[] coord = {0,0};
 //
 //FillEmptyArray(matrix);
 //FillArray();
 //Print2DArray(matrix);
 SpiralPath();
-Print2DArray2(mas);
-
-// Console.WriteLine();
+Print2DArray(mas);
 
 // Функции
 void SpiralPath()
@@ -18,7 +15,6 @@ void SpiralPath()
     int y = 0;
     int sx = 1; // смещение x
     int sy = 0; // смещение y
-    Console.WriteLine($"mas[{y},{x}]");
     int countNum = 1;
     mas[y,x] = countNum;
     bool stop;
@@ -32,14 +28,13 @@ void SpiralPath()
             y+=sy;
             countNum++;
             mas[y,x] = countNum;
-            Console.WriteLine($"mas[{y},{x}]={countNum}");
+            //Console.WriteLine($"mas[{y},{x}]={countNum}");
             stop = true;
         }
         int sx_old = sx;
         sx = -sy;
         sy = sx_old;
         countCicle++;
-        Console.WriteLine($"countCicle={countCicle}");
     }while(stop);
     
 }
@@ -49,75 +44,73 @@ bool InRange(int col, int row, int count)
         return false;
     if(row < 0 || row >= N)
         return false;
-    if(count > 20)
-        return false;
     if(mas[row,col]>0)
         return false;
     return true;
 }
-void FillArray()
-{
-    int count = 1; 
-    for(int step = 0; step < 1; step++)
-    {
-        // columns
-        for(int i = 0; i< matrix.GetLength(1); i++)
-        {
-            matrix[0,i] = $"0{count++}"; 
-        }
-        // rows
-        for(int i = 1; i< matrix.GetLength(0); i++)
-        {
-            matrix[i,matrix.GetLength(1)-1] = $"0{count++}"; 
-        }
-        // columns
-        for(int i = matrix.GetLength(1)-2; i > 0; i--)
-        {
-            matrix[matrix.GetLength(0)-1,i] = $"0{count++}"; 
-        }
-        // rows
-        for(int i = matrix.GetLength(0)-1; i > 0; i--)
-        {
-            matrix[i,0] = $"0{count++}"; 
-        }  
+// void FillArray()
+// {
+//     int count = 1; 
+//     for(int step = 0; step < 1; step++)
+//     {
+//         // columns
+//         for(int i = 0; i< matrix.GetLength(1); i++)
+//         {
+//             matrix[0,i] = $"0{count++}"; 
+//         }
+//         // rows
+//         for(int i = 1; i< matrix.GetLength(0); i++)
+//         {
+//             matrix[i,matrix.GetLength(1)-1] = $"0{count++}"; 
+//         }
+//         // columns
+//         for(int i = matrix.GetLength(1)-2; i > 0; i--)
+//         {
+//             matrix[matrix.GetLength(0)-1,i] = $"0{count++}"; 
+//         }
+//         // rows
+//         for(int i = matrix.GetLength(0)-1; i > 0; i--)
+//         {
+//             matrix[i,0] = $"0{count++}"; 
+//         }  
         
-        ////// repeat  /////
-        // columns
-        for(int i = 1; i< matrix.GetLength(1)-1; i++)
-        {
-            matrix[1,i] = $"0{count++}"; 
-        }
-        // columns
-        for(int i = matrix.GetLength(1)-2; i > 0; i--)
-        {
-            matrix[matrix.GetLength(0)-1-1,i] = $"0{count++}"; 
-        }
-    }
-}
+//         ////// repeat  /////
+//         // columns
+//         for(int i = 1; i< matrix.GetLength(1)-1; i++)
+//         {
+//             matrix[1,i] = $"0{count++}"; 
+//         }
+//         // columns
+//         for(int i = matrix.GetLength(1)-2; i > 0; i--)
+//         {
+//             matrix[matrix.GetLength(0)-1-1,i] = $"0{count++}"; 
+//         }
+//     }
+// }
 
-void FillEmptyArray(string[,] array)
-{
-    for(int i = 0; i < array.GetLength(0); i++)
-    {
-        for(int j = 0; j < array.GetLength(1); j++)
-        {            
-            array[i,j] = String.Empty;
-        }
-    }
-}
+// void FillEmptyArray(string[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {            
+//             array[i,j] = String.Empty;
+//         }
+//     }
+// }
 
-void Print2DArray(string[,] array)
-{
-    for(int i = 0; i < array.GetLength(0); i++)
-    {
-        for(int j = 0; j < array.GetLength(1); j++)
-        {            
-            Console.Write($"{array[i,j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-void Print2DArray2(int[,] array)
+// void Print2DArray(string[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {            
+//             Console.Write($"{array[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+void Print2DArray(int[,] array)
 {
     for(int i = 0; i < array.GetLength(0); i++)
     {
